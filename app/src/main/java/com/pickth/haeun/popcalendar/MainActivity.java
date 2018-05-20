f0fae2146456f5341ce4914d6699fde1f1db08a9
HaEun Kim <hagomy@naver.com>
2018-05-20 20:37:56 +0900

    Add month's

---
+++ app/src/main/java/com/pickth/haeun/popcalendar/MainActivity.java
@@ -0,0 +1,84 @@
+package com.pickth.haeun.popcalendar;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.TextView;
+
+/**
+ * Created by HaEun on 2018-05-18.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    GridView monthView; //월별 캘린더 뷰 객체
+    MonthAdapter monthViewAdapter;  //월별 캘린더 어댑터
+    TextView monthText; //월을 표시하는 텍스트뷰
+
+    int curYear;    //현재 연도
+    int curMonth;   //현재 월
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // 월별 캘린더 뷰 객체 참조
+        monthView = (GridView) findViewById(R.id.gv_main_monthView);
+        monthViewAdapter = new MonthAdapter(this);
+        monthView.setAdapter(monthViewAdapter);
+
+        // 리스너 설정
+        monthView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                // 현재 선택한 일자 정보 표시
+                MonthItem curItem = (MonthItem) monthViewAdapter.getItem(position);
+                int day = curItem.getDay();
+
+                Log.d("MainActivity", "Selected : " + day);
+            }
+        });
+
+
+        monthText = (TextView) findViewById(R.id.tv_main_monthText);
+        setMonthText();
+
+        // 이전 월로 넘어가는 이벤트 처리
+        Button monthPrevious = (Button) findViewById(R.id.btn_main_monthPrevious);
+        monthPrevious.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                monthViewAdapter.setPreviousMonth();
+                monthViewAdapter.notifyDataSetChanged();
+
+                setMonthText();
+            }
+        });
+
+        // 다음 월로 넘어가는 이벤트 처리
+        Button monthNext = (Button) findViewById(R.id.btn_main_monthNext);
+        monthNext.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                monthViewAdapter.setNextMonth();
+                monthViewAdapter.notifyDataSetChanged();
+
+                setMonthText();
+            }
+        });
+
+    }
+
+    /**
+     * 월 표시 텍스트 설정
+     */
+    private void setMonthText() {
+        curYear = monthViewAdapter.getCurYear();
+        curMonth = monthViewAdapter.getCurMonth();
+
+        monthText.setText(curYear + "년 " + (curMonth + 1) + "월");
+    }
+}
