a6b0ed57f2690a858e226ca6e13ab96ef13637e5
HaEun Kim <hagomy@naver.com>
2018-06-01 12:03:34 +0900

    Modify actionbar

--- app/src/main/java/com/pickth/haeun/popcalendar/AddActivity.java
+++ app/src/main/java/com/pickth/haeun/popcalendar/AddActivity.java
@@ -3,6 +3,8 @@ package com.pickth.haeun.popcalendar;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 /**
  * Created by HaEun on 2018-05-28.
@@ -13,5 +15,21 @@ public class AddActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.actionbar_actions, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_add :
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
