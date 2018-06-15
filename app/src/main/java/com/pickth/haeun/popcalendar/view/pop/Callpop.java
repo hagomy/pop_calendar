c179d828b29b213a5ca82d07da3e9e8d81196723
Jinsil117 <jinsil117@nate.com>
2018-06-15 14:14:28 +0900

    add change

--- app/src/main/java/com/pickth/haeun/popcalendar/view/pop/Callpop.java
+++ app/src/main/java/com/pickth/haeun/popcalendar/view/pop/Callpop.java
@@ -1,22 +1,13 @@
 package com.pickth.haeun.popcalendar.view.pop;
 
-import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.WindowManager;
-import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.PopupWindow;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.pickth.haeun.popcalendar.R;
-import com.pickth.haeun.popcalendar.view.calendar.CalendarActivity;
 
 import static android.view.View.inflate;
 
@@ -25,17 +16,22 @@ import static android.view.View.inflate;
  */
 
 public class Callpop extends AppCompatActivity {
+    String phoneNumber;
 
     protected  void  onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.item_pop);
     }
 
-    LinearLayout container = (LinearLayout)findViewById(R.id.page);
+    public Callpop(Context context, String phone){
+        phoneNumber = phone;
+    }
+
+    LinearLayout container = (LinearLayout)findViewById(R.id.ll_pop_page);
     View view = inflate(this, R.layout.item_pop, null);
     //container.addView(view);
 
-    TextView textView = (TextView)container.findViewById(R.id.popup);
+    TextView textView = (TextView)container.findViewById(R.id.tv_pop_sc);
 
 //일정이 있습니다, 버튼 생성 후 인프레이터로 연결
 
